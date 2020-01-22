5fbf79dac7d756a73b10b80f3db39cc5e9400204
Alexis Delhaie <alexis.delhaie@yahoo.com>
2020-01-22 16:24:58 +0100

    first commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+let assert = require('assert');
+
+describe("helloWorld", function () {
+    describe('#indexOf()', function () {
+        it('should return true if true is true', function () {
+            assert.equal(true, true);
+        });
+    })
+});
