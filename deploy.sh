30fd5e8dc5ad042a77df423013b4601db8a46a6a
Alexis Delhaie <alexis.delhaie@yahoo.com>
2020-01-23 10:05:10 +0100

    test de ses morts 4

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
-  rm -rf "$HOME/google-cloud-sdk"
-  curl https://sdk.cloud.google.com | bash > /dev/null
-fi
-
-gcloud components update kubectl
 make gauth build push deploy
